f9f6bb2b68b11beb0b97533f7f352acebc8edf32
Eduardo Sandoval <edubizzle7@gmail.com>
2020-12-11 16:30:20 -0600

    se agrego la conexion a la bd

---
+++ ccc_nodejs/build/server.js
@@ -0,0 +1,28 @@
+let express = require("express");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+let app = express();
+const morgan = require("morgan");
+// Settings
+let port = process.env.PORT || 3000; //puerto donde correrá
+
+app.use(cors());
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: false
+//     })
+// );
+
+// Middlewares
+app.use(morgan("dev"));
+//app.use('/usuarios', Users);
+app.use(express.json());
+
+//ROUTES--------------------------------------------------------------
+
+// app.use("/api/user", require("./routes/Users"));
+
+// Start listening
+app.listen(port, function () {
+  console.log("Server is running on port: " + port);
+});
