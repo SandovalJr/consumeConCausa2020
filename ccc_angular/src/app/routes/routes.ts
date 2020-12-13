215fd7dcc2f62ceea831db53532a66de67f13904
Eduardo Sandoval <edubizzle7@gmail.com>
2020-12-13 01:30:05 -0600

    componentes de los formularios

--- ccc_angular/src/app/routes/routes.ts
+++ ccc_angular/src/app/routes/routes.ts
@@ -2,7 +2,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { Component } from "@angular/core";
 import { SpaComponent } from "../components/spa/spa.component";
 import { LoginComponent } from "../components/sw/login/login.component";
+
+// REGISTROS
 import { RegistrosComponent } from "../components/sw/registros/registros.component";
+import { RegistroEmpresaComponent } from "../components/sw/registros/registro-empresa/registro-empresa.component";
+import { RegistroClientesComponent } from "../components/sw/registros/registro-clientes/registro-clientes.component";
+
 const routes: Routes = [
   { path: "", component: SpaComponent },
   {
@@ -12,6 +17,14 @@ const routes: Routes = [
   {
     path: "Resgistrate",
     component: RegistrosComponent,
+  },
+  {
+    path: "Registrate_Empresa",
+    component: RegistroEmpresaComponent,
+  },
+  {
+    path: "Registrate_Cliente",
+    component: RegistroClientesComponent,
   },
 ];
 
